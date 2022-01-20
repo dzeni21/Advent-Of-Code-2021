dccc4ec59dbab93d30692a99a0be82680ffe1413
dzeni21 <85407660+dzeni21@users.noreply.github.com>
2022-01-20 20:36:28 +0100

    Update dan19.py

--- dan19.py
+++ dan19.py
@@ -1,102 +1,176 @@
-from math import dist
+from functools import lru_cache
+from itertools import combinations
+from collections import defaultdict
 
 with open("dan19.txt") as f:
     input = f.read().strip()
+    input = input.split("\n")
 
-redovi = []
-pom = []
-scanners = []
+# Parse
+scanner = []
+i = 0
+while i < len(input):
+    beacon = []
+    while i < len(input) and len(input[i]) > 0:
+        if "--- scanner" in input[i]:
+            i += 1
+            continue
+        beacon.append(tuple([int(i)
+                       for i in input[i].split(",")]))
+        i += 1
+    scanner.append(tuple(sorted(beacon)))
+    i += 1
 
-for red in input.stdin:
-    redovi.append(red.strip())
+@lru_cache(None)
+def inv(rot):
+    a = rotacije((1, 2, 3))[rot]
+    for inv_rot in range(24):
+        if rotacije(a)[inv_rot] == (1, 2, 3):
+            return inv_rot
 
-for x in redovi:
-    tup = x.split(",")
-    if len(tup) == 3:
-        pom.append(tuple(map(int, tup)))
-    elif pom:
-        scanners.append(pom)
-        pom = []
-scanners.append(pom)
 
-def genSve(scanner):
-    def pomFja(scanner):
-        pom1 = [(x, y, z) for x, y, z in scanner]
-        pom2 = [(x, -z, y) for x, y, z in scanner]
-        pom3 = [(x, -y, -z) for x, y, z in scanner]
-        pom4 = [(x, z, -y) for x, y, z in scanner]
-        return [pom1, pom2, pom3, pom4]
-    ret = pomFja(scanner)
-    ret.extend(pomFja([(-x, z, y) for x, y, z in scanner]))
-    ret.extend(pomFja([(y, z, x) for x, y, z in scanner]))
-    ret.extend(pomFja([(-y, x, z) for x, y, z in scanner]))
-    ret.extend(pomFja([(z, y, -x) for x, y, z in scanner]))
+@lru_cache(None)
+def compose(rot1, rot2):
+    a = rotacije(rotacije((1, 2, 3))[rot1])[rot2]
+    for comp_rot in range(24):
+        if rotacije((1, 2, 3))[comp_rot] == a:
+            return comp_rot
 
-    ret.extend(pomFja([(-z, x, -y) for x, y, z in scanner]))
-    return ret
+def sub(x, y):
+    return (x[0] - y[0], x[1] - y[1], x[2] - y[2])
 
-sveOrijentacije = list(map(genSve, scanners))
+def add(x, y):
+    return (x[0] + y[0], x[1] + y[1], x[2] + y[2])
 
-def dajMatch(scanner1, scanner2):
-    gen1 = sveOrijentacije[scanner1]
-    gen2 = sveOrijentacije[scanner2]
-    ret = dict()
-    for orijentacija1 in range(24):
-        for orijentacija2 in range(24):
-            d = {}
-            for i in gen1[orijentacija1]:
-                for j in gen2[orijentacija2]:
-                    triple = tuple(map(lambda x: i[x] - j[x], range(3))) + (scanner1*24 + orijentacija1, scanner2*24 + orijentacija2)
-                    d[triple] = d.get(triple, 0) + 1
-            provjera = list(filter(lambda arg: arg[1] >= 12, d.items()))
-            if provjera:
-                ret[provjera[0][0][-2]] = (provjera[0][0][-1], provjera[0][0][:3])
-    return ret
+def neg(x):
+    return (-x[0], -x[1], -x[2])
 
-mem = dict()
-preklapanje = dict()
-for i in range(len(scanners)):
-    for j in range(i + 1, len(scanners)):
-        if (i, j) not in mem:
-            provjera = dajMatch(i, j)
-            mem[(i, j)] = provjera
-        else:
-            provjera = mem[(i, j)]
-        if provjera:
-            preklapanje[i] = preklapanje.get(i, []) + [j]
-            preklapanje[j] = preklapanje.get(j, []) + [i]
+def manhattanDist(x, y):
+    return sum([abs(x[i] - y[i]) for i in range(3)])
 
-pos = {0: (0, (0, 0, 0))}
-visible = [False] * len(scanners)
-r = [0]
-while r:
-    i = r.pop()
-    visible[i] = True
-    for j in preklapanje[i]:
-        if not visible[j]:
-            if (i, j) not in mem:
-                provjera = dajMatch(i, j)
-                mem[(i, j)] = provjera
-            else:
-                provjera = mem[(i, j)]
-            if pos[i][0] in provjera:
-                r.append(j)
-                pos[j] = (provjera[pos[i][0]][0], tuple(map(sum, zip(provjera[pos[i][0]][1], pos[i][1]))))
+@lru_cache(None)
+def rotacije(tacka):
+    x, y, z = tacka
+    return [
+        (x, y, z), (x, z, -y), (x, -y, -z), (x, -z, y),
+        (-x, -y, z), (-x, z, y), (-x, y, -z), (-x, -z, -y),
+        (y, z, x), (y, x, -z), (y, -z, -x), (y, -x, z),
+        (-y, -z, x), (-y, x, z), (-y, z, -x), (-y, -x, -z),
+        (z, x, y), (z, y, -x), (z, -x, -y), (z, -y, x),
+        (-z, -x, y), (-z, y, x), (-z, x, -y), (-z, -y, -x)
+    ]
 
-beacons = set(scanners[0])
-for i in range(1, len(scanners)):
-    for j in sveOrijentacije[i][pos[i][0] % 24]:
-        beacons.add(tuple(map(sum, zip(j, pos[i][1]))))
+def hash(a):
+    return tuple(sorted([abs(i) for i in a]))
 
-p1 = len(beacons)
-print("P1: ", p1)
 
-manhattanDist = []
-for i in range(len(scanners)):
-    for j in range(i + 1, len(scanners)):
-        ctr1 = pos[i][1]
-        ctr2 = pos[j][1]
-        manhattanDist.append(sum(abs(ctr1[k] - ctr2[k]) for k in range(3)))
+def offset_set(beacon):
+    rez = set()
+    for x, y in combinations(beacon, r=2):
+        rez.add(sub(x, y))
+    return rez
 
-p2 = max(manhattanDist)
-print("P2: ", p2)
+
+@lru_cache(None)
+def udaljenost_set(beacon):
+    return set([hash(x) for x in offset_set(beacon)])
+
+
+def checkOverlap(a, b):
+    if len(set.intersection(udaljenost_set(a), udaljenost_set(b))) >= 66:
+        return True
+    return False
+
+
+def orientacija(a, b, base_index, rots):
+    a_set = set(a)
+
+    for rot in rots:
+        drugiBeacon = tuple([rotacije(beacon)[rot] for beacon in b])
+        for drugiBase in drugiBeacon:
+            translacija = sub(a[base_index], drugiBase)
+            drugiBeacon_translated = set(
+                [add(beacon, translacija) for beacon in drugiBeacon])
+
+            if len(set.intersection(a_set, drugiBeacon_translated)) >= 12:
+                return translacija, rot
+
+    return None
+
+def overlap(a, b):
+    if not checkOverlap(a, b):
+        return False
+
+    for i, j in combinations(range(len(a)), r=2):
+        for k, m in combinations(range(len(b)), r=2):
+            da = sub(a[i], a[j])
+            db = sub(b[k], b[m])
+            if not hash(da) == hash(db):
+                continue
+
+            rot = []
+
+            rot_db = rotacije(db)
+            for i in range(24):
+                if rot_db[i] == da:
+                    rot.append(i)
+                    break
+            db = neg(db)
+            rot_db = rotacije(db)
+            for i in range(24):
+                if rot_db[i] == da:
+                    rot.append(i)
+                    break
+
+            if len(rot) == 0:
+                continue
+
+            return orientacija(a, b, i, rot)
+
+    return False
+
+
+adj = defaultdict(list)
+for i in range(len(scanner)):
+    for j in range(i + 1, len(scanner)):
+        x = overlap(scanner[i], scanner[j])
+        if x:
+            adj[i].append((j, x[0], x[1]))
+            adj[j].append((i, rotacije(neg(x[0]))[inv(x[1])], inv(x[1])))
+
+
+beacon = set()
+
+scannersPairs = {}
+stek = [(0, (0, 0, 0), 0)]
+posjeta = set()
+while len(stek) > 0:
+    cvor, trans, rot = stek.pop()
+
+    if cvor in posjeta:
+        continue
+    posjeta.add(cvor)
+
+    scannersPairs[cvor] = trans
+
+    trenutni_beacon = [add(rotacije(beacon)[rot], trans)
+                   for beacon in scanner[cvor]]
+
+    for b in trenutni_beacon:
+        beacon.add(b)
+
+    for nbr in adj[cvor]:
+        if nbr[0] in posjeta:
+            continue
+        nTrans = add(trans, rotacije(nbr[1])[rot])
+        nRot = compose(nbr[2], rot)
+        stek.append((nbr[0], nTrans, nRot))
+
+print("P1: ", len(beacon))
+
+
+maxDist = 0
+for a, b in combinations(scannersPairs.values(), r=2):
+    maxDist = max(maxDist, manhattanDist(a, b))
+
+print("P2: ", maxDist)
