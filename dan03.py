3464038e16e08f0b02b98131940256c6c6ba58fa
dzeni21 <85407660+dzeni21@users.noreply.github.com>
2022-01-20 14:29:00 +0100

    Update dan03.py

--- dan03.py
+++ dan03.py
@@ -1,59 +1,56 @@
 with open("dan03.txt") as f:
     input = f.read().strip().split("\n")
 
-def binToDec(broj):
-    ret = 0
-    for i in broj:
-        ret *= 2
-        ret += int(i)
-    return ret
+brojBin = len(input[0])  
 
-bits1 = []
-pom = []
-
-for red in range(1, len(input)):
-    bits1.append(red.strip())
+gama = [None] * brojBin
+epsilon = [None] * brojBin
+for i in range(brojBin):
+    nule = sum([input[j][i] == "0" for j in range(len(input))])
+    jedinice = sum([input[j][i] == "1" for j in range(len(input))])
+    if nule > jedinice:
+        epsilon[i] = "1"
+        gama[i] = "0"
+    else:
+        epsilon[i] = "0"
+        gama[i] = "1"
 
 # P1
-for red in bits1:
-    if not pom:
-        pom = list(map(int, red))
+p1 = int("".join(gama), 2) * int("".join(epsilon), 2)
+print("P1: ",p1)
+
+
+# Odredi broj bita
+N = len(input[0])
+input = [int(i, 2) for i in input]
+
+# kisik
+kisik = input.copy()
+poz = N - 1
+while poz >= 0 and len(kisik) > 1:
+    # najcesci bit
+    jedinice = sum([((x & (1 << poz)) >> poz) for x in kisik])
+    nule = len(kisik) - jedinice
+
+    if nule > jedinice:
+        kisik = list(filter(lambda arg: not (arg & (1 << poz)), kisik))
     else:
-        for i in range(len(red)):
-            pom[i] += int(red[i])
+        kisik = list(filter(lambda arg: (arg & (1 << poz)), kisik))
+    poz -= 1
 
-n = len(bits1)
-gamma = str().join(list(map(lambda arg: str(int(arg > n/2)), pom)))
-epsilon = str().join(list(map(lambda arg: str(int(arg < n/2)), pom)))
-x1 = binToDec(gamma)
-x2 = binToDec(epsilon)
-
-print("P1:", x1 * x2)
-
-# P2
-b = len(bits1[0])
-bits2 = bits1.copy()
-for i in range(b):
-    if len(bits1) == 1:
-        break
-    # oxygen i co2 rating
-    brojac = [0, 0]
-    for bit in bits1:
-        brojac[int(bit[i])] += 1
-    if brojac[0] > brojac[1]:
-        bits1 = list(filter(lambda arg: arg[i] == "0", bits1))
+# CO2
+co2 = input.copy()
+poz = N - 1
+while poz >= 0 and len(co2) > 1:
+    # najcesci bit
+    jedinice = sum([((x & (1 << poz)) >> poz) for x in co2])
+    nule = len(co2) - jedinice
+    if nule > jedinice:
+        co2 = list(filter(lambda x: (x & (1 << poz)), co2))
     else:
-        bits1 = list(filter(lambda arg: arg[i] == "1", bits1))
+        co2 = list(filter(lambda x: not (x & (1 << poz)), co2))
+    poz -= 1
 
-for i in range(b):
-    if len(bits2) == 1:
-        break
-    brojac = [0, 0]
-    for bit in bits2:
-        brojac[int(bit[i])] += 1
-    if brojac[0] > brojac[1]:
-        bits2 = list(filter(lambda arg: arg[i] == "1", bits2))
-    else:
-        bits2 = list(filter(lambda arg: arg[i] == "0", bits2))
 
-print("P2: ", binToDec(bits1[0]) * binToDec(bits2[0]))
+p2 = kisik[0] * co2[0]
+print("P2: ",p2)
