8dc2138707fece07aaf57d9f4b3c32e3e4604b9b
dzeni21 <85407660+dzeni21@users.noreply.github.com>
2022-01-20 19:32:27 +0100

    Update dan18.py

--- dan18.py
+++ dan18.py
@@ -1,72 +1,172 @@
-import itertools
-import math
-from functools import reduce
-
 with open("dan18.txt") as f:
-    input = f.read().strip()
+    input = f.read().strip().split("\n")
+podaci = [eval(line) for line in input]
+
+# Prikazujemo podatke kao stablo
+class Cvor:
+    def __init__(s, val=None):
+        s.val = val
+        s.left = None
+        s.right = None
+        s.par = None
+
+    def __str__(s):
+        if isinstance(s.val, int):
+            return str(s.val)
+        return f"[{str(s.left)},{str(s.right)}]"
 
 
-def dodajLijevo(x, n):
-    if n is None:
-        return x
-    if isinstance(x, int):
-        return x + n
-    return [dodajLijevo(x[0], n), x[1]]
+def parse(brFish):
+    korijen = Cvor()
+    if isinstance(brFish, int):
+        korijen.val = brFish
+        return korijen
+
+    korijen.left = parse(brFish[0])
+    korijen.right = parse(brFish[1])
+    korijen.left.par = korijen
+    korijen.right.par = korijen
+
+    smanji(korijen)
+
+    return korijen
 
 
-def dodajDesno(x, n):
-    if n is None:
-        return x
-    if isinstance(x, int):
-        return x + n
-    return [x[0], dodajDesno(x[1], n)]
+def dodaj(a, b):
+# dodaj dva stabla
+    korijen = Cvor()
+    korijen.left = a
+    korijen.right = b
+    korijen.left.par = korijen
+    korijen.right.par = korijen
+    smanji(korijen)
+    return korijen
 
 
-def checkEksplozija(x, n = 4):
-    if isinstance(x, int):
-        return False, None, x, None
-    if n == 0:
-        return True, x[0], 0, x[1]
-    a, b = x
-    exp, left, a, right = checkEksplozija(a, n - 1)
-    if exp:
-        return True, left, [a, dodajLijevo(b, right)], None
-    exp, left, b, right = checkEksplozija(b, n - 1)
-    if exp:
-        return True, None, [dodajDesno(a, left), b], right
-    return False, None, x, None
+def magnituda(korijen):
+    if isinstance(korijen.val, int):
+        return korijen.val
+
+    return 3 * magnituda(korijen.left) + 2 * magnituda(korijen.right)
 
 
-def split(x):
-    if isinstance(x, int):
-        if x >= 10:
-            return True, [x // 2, math.ceil(x / 2)]
-        return False, x
-    a, b = x
-    promjena, a = split(a)
-    if promjena:
-        return True, [a, b]
-    promjena, b = split(b)
-    return promjena, [a, b]
+def smanji(korijen):
+    checkDone = True
 
+    stek = [(korijen, 0)]
 
-def add(a, b):
-    x = [a, b]
-    while True:
-        promjena, _, x, _ = checkEksplozija(x)
-        if promjena:
+    while len(stek) > 0: 
+        Cvor, dubina = stek.pop()
+
+        if Cvor == None:
             continue
-        promjena, x = split(x)
-        if not promjena:
+
+        stanje = (Cvor.left == None and Cvor.right == None) or (
+            Cvor.left.val != None and Cvor.right.val != None)
+
+        if dubina >= 4 and Cvor.val == None and stanje:
+            # Idi uz stek i pronađi lijevi cvor
+            prev_Cvor = Cvor.left
+            cur_Cvor = Cvor
+            while cur_Cvor != None and (cur_Cvor.left == prev_Cvor or cur_Cvor.left == None):
+                prev_Cvor = cur_Cvor
+                cur_Cvor = cur_Cvor.par
+
+            # Lijevi cvor mora da postoji
+            if cur_Cvor != None:
+
+                cur_Cvor = cur_Cvor.left
+                while cur_Cvor.val == None:
+                    if cur_Cvor.right != None:
+                        cur_Cvor = cur_Cvor.right
+                    else:
+                        cur_Cvor = cur_Cvor.left
+
+                # azuriraj vrijednosti
+                cur_Cvor.val += Cvor.left.val
+
+            # Idi uz stek i pronađi desni cvor
+            prev_Cvor = Cvor.right
+            cur_Cvor = Cvor
+            while cur_Cvor != None and (cur_Cvor.right == prev_Cvor or cur_Cvor.right == None):
+                prev_Cvor = cur_Cvor
+                cur_Cvor = cur_Cvor.par
+
+            # desni cvor mora da postoji
+            if cur_Cvor != None:
+
+                cur_Cvor = cur_Cvor.right
+                while cur_Cvor.val == None:
+                    if cur_Cvor.left != None:
+                        cur_Cvor = cur_Cvor.left
+                    else:
+                        cur_Cvor = cur_Cvor.right
+
+                # azuriraj vrijednosti
+                cur_Cvor.val += Cvor.right.val
+
+            Cvor.val = 0
+            Cvor.left = None
+            Cvor.right = None
+
+            checkDone = False
             break
-    return x
 
+        stek.append((Cvor.right, dubina + 1))
+        stek.append((Cvor.left, dubina + 1))
 
-def magnituda(x):
-    if isinstance(x, int):
-        return x
-    return 3 * magnituda(x[0]) + 2 * magnituda(x[1])
+    if not checkDone:
+        smanji(korijen)
+        return
 
+    stek = [korijen]
+    while len(stek) > 0:
+        Cvor = stek.pop()
+        if Cvor == None:
+            continue
 
-print("P1: ", magnituda(reduce(add, input)))
-print("P2: ",  max(magnituda(add(a, b)) for a, b in itertools.permutations(input, 2)),)
+        if Cvor.val != None:
+            # split
+            assert Cvor.left == None and Cvor.right == None
+            if Cvor.val >= 10:
+                Cvor.left = Cvor(Cvor.val//2)
+                Cvor.right = Cvor(Cvor.val - (Cvor.val//2))
+                Cvor.left.par = Cvor
+                Cvor.right.par = Cvor
+                Cvor.val = None
+
+                checkDone = False
+                break
+
+        stek.append(Cvor.right)
+        stek.append(Cvor.left)
+
+    # ako nije checkDone, nastavi
+    if not checkDone:
+        smanji(korijen)
+
+korijen = parse(podaci[0])
+
+i = 1
+while i < len(podaci):
+    korijen = dodaj(korijen, parse(podaci[i]))
+    i += 1
+
+p1 = magnituda(korijen)
+print("P1: ", p1)
+
+def dajMag(a, b):
+    return magnituda(dodaj(a, b))
+
+p2 = 0
+for i in range(len(podaci)):
+    for j in range(len(podaci)):
+        if i == j:
+            continue
+
+        a, b = parse(podaci[i]), parse(podaci[j])
+
+        if dajMag(a, b) > ans:
+            ans = dajMag(a, b)
+
+print("P2: ", p2)
