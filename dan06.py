b94797ee69b8eafa00543a2161652ecacc572d67
dzeni21 <85407660+dzeni21@users.noreply.github.com>
2022-01-20 22:43:38 +0100

    Update dan06.py

--- dan06.py
+++ dan06.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from pprint import pprint
+
 with open("dan06.txt") as f:
     input = f.read().strip().split(",")
     podaci = [int(i) for i in input]
@@ -15,3 +18,29 @@ for _ in range(days):
 
 p1 = len(podaci)
 print(p1)
+
+
+with open("dan06.txt") as f:
+    input = f.read().strip().split(",")
+    ponavljanja = defaultdict(int)
+    for i in input:
+        ponavljanja[int(i)] += 1
+
+dani = 256
+
+for _ in range(days):
+    newPonavljanje = defaultdict(int)
+
+    for key in ponavljanja:
+        if key == 0:
+            newPonavljanje[6] += ponavljanja[key]
+            newPonavljanje[8] = ponavljanja[key]
+        else:
+            newPonavljanje[key - 1] += ponavljanja[key]
+
+    ponavljanja = newPonavljanje
+
+p2 = 0
+for key in ponavljanja:
+    p2 += ponavljanja[key]
+print(p2)
