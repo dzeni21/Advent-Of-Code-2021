c3443d0a1a45e5f51f4918613649a242f0f51403
dzeni21 <85407660+dzeni21@users.noreply.github.com>
2022-01-20 17:44:03 +0100

    Update dan21.py

--- dan21.py
+++ dan21.py
@@ -1,57 +1,112 @@
+from functools import lru_cache
+
 with open("dan21.txt") as f:
     input = f.read().strip().split("\n")
 
-player1, player2 = int(input.strip()[-1]), int(input.strip()[-1])
 
-def wrap(br, ost):
-    if br % ost == 0:
-        return ost
-    return br % ost
+class Igrac:
+    def __init__(player, poz):
+        player.poz = poz
+        player.poeni = 0
 
-def igra(dice, player1, player2):
-    bodovi1, bodovi2, brojac = 0, 0, 0
-    while bodovi1 < 1000 and bodovi2 < 1000:
-        player1 = wrap(player1 + 3 * dice, 10)
-        dice = wrap(dice + 3, 100)
-        brojac, bodovi1 = brojac + 3, bodovi1 + player1
-        if bodovi1 >= 1000:
-            break
-        player2 = wrap(player2 + 3 * dice, 10)
-        dice = wrap(dice + 3, 100)
-        brojac, bodovi2 = brojac + 3, bodovi2 + player2
-    return (bodovi1, bodovi2, brojac)
-p1 = igra(2, player1, player2)
-print("P1: ", min(p1[:2]) * p1[-1])
+    def unaprijed(player, die):
+        player.poz += die.roll()
+        player.poz = (player.poz - 1) % 10 + 1
+        player.poeni += player.poz
 
-mem = dict()
-cts = dict()
-for i in range(3):
-    for j in range(3):
-        for k in range(3):
-            cts[i + j + k + 3] = cts.get(i + j + k + 3, 0) + 1
+    def pobjeda(player):
+        return player.poeni >= 1000
 
-LIMIT = 21
 
-def igraP2(player1, player2, bodovi1, bodovi2, red):
-    if (player1, player2, bodovi1, bodovi2, red) in mem:
-        return mem[(player1, player2, bodovi1, bodovi2,red)]
-    stanje = {0: 0, 1: 0}
-    if bodovi1 >= LIMIT or bodovi2 >= LIMIT:
-        stanje[int(bodovi2 > bodovi1)] += 1
-        mem[(player1, player2, bodovi1, bodovi2,red)] = stanje
-        return stanje
+class Dice:
+    def __init__(player):
+        player.val = 1
+        player.rolls = 0
 
-    for die in range(3, 10):
-        if not red:
-            newPlayer1 = wrap(player1 + die, 10)
-            pom = igraP2(newPlayer1, player2, bodovi1 + newPlayer1, bodovi2, 1 - red)
-        else:
-            newPlayer2 = wrap(player2 + die, 10)
-            pom = igraP2(player1, newPlayer2, bodovi1, bodovi2 + newPlayer2, 1 - red)
-        for i in range(2):
-            stanje[i] += cts[die] * pom[i]
-    mem[(player1, player2, bodovi1, bodovi2, red)] = stanje
-    return stanje
-p2 = igraP2(player1, player2, bodovi1=0, bodovi2=0,red=0)
+    def roll(player, times = 3):
+        s = 0
+        for _ in range(times):
+            s += player.val
+            player.val += 1
+        player.rolls += times
+        return s
 
-print("P2: ", max(p2.values()))
+d = Dice()
+p1 = Igrac(int(input[0][input[0].index(": ")+2:]))
+p2 = Igrac(int(input[1][input[1].index(": ")+2:]))
+
+while True:
+    p1.unaprijed(d)
+    if p1.pobjeda():
+        odg1 = p2.poeni * d.rolls
+        break
+
+    p2.unaprijed(d)
+    if p2.pobjeda():
+        odg1 = p1.poeni * d.rolls
+        break
+
+print("P1: ", odg1)
+
+
+p1 = int(input[0][input[0].index(": ")+2:])
+p2 = int(input[1][input[1].index(": ")+2:])
+
+freq = {
+    3: 1,
+    4: 3,
+    5: 6,
+    6: 7,
+    7: 6,
+    8: 3,
+    9: 1
+}
+
+
+def unaprijed(poz, kolicina):
+    return (poz + kolicina - 1) % 10 + 1
+
+
+def unazad(poz, kolicina):
+    return (poz - kolicina - 1) % 10 + 1
+
+
+@lru_cache(None)
+def dp(poz, poeni, red, init_poz):
+    # Koliko postoji nacina da se dodje do odredjene tacke, na datu poziciju
+    if red == 0:
+        return 1 if (poeni == 0 and poz == init_poz) else 0
+
+    if poeni <= 0:
+        return 0
+
+    odg2 = 0
+    for kolicina in freq:
+        if poeni - poz >= 21:
+            continue
+
+        dpVal = dp(unazad(poz, kolicina),
+                    poeni - poz, red - 1, init_poz)
+        odg2 += freq[kolicina] * dpVal
+
+    return odg2
+
+
+# Broj nacina do pobjede
+def countPobjede(poz, drugaPoz, jeP1):
+    odg2 = 0
+    for endPoz in range(1, 11):
+        for poeni in range(21, 31):
+            for red in range(40):
+                for drugiEnd_poz in range(1, 11):
+                    for drugiPoeni in range(21):
+                        odg2 += dp(endPoz, poeni, red, poz) * \
+                        dp(drugiEnd_poz, drugiPoeni,
+                               red - jeP1, drugaPoz)
+
+    return odg2
+
+
+p1 = countPobjede(p1, p2, True)
+p2 = countPobjede(p2, p1, False)
+print(max(p1, p2))
