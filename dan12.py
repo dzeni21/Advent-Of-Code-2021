8bef38ff174fad0c3a2ffcddc80ec2aa15fc2d07
dzeni21 <85407660+dzeni21@users.noreply.github.com>
2022-01-20 17:16:24 +0100

    Update dan12.py

--- dan12.py
+++ dan12.py
@@ -1,29 +1,87 @@
+from collections import defaultdict, deque
+from pprint import pprint
+
 with open("dan12.txt") as f:
     input = f.read().strip()
+podaci = [i.split("-") for i in input.split("\n")]
 
-assert len(input.argv) == 2
-rubovi = list(map(lambda arg: arg.split('-'), open(input.argv[1]).read().splitlines()))
 
-def countPath(graph, checkDoublePath, node = 'start', seen = ['start']):
-    if node == 'end':
-        return 1
-    count = 0
-    for susjed in graph[node]:
-        if susjed != 'start':
-            if susjed[0].isupper() or susjed not in seen:
-                count += countPath(graph, checkDoublePath, susjed, seen + [susjed])
-            elif not checkDoublePath:
-                count += countPath(graph, True, susjed, seen)
-    return count
+def jeMalo(cave):
+    return cave.islower()
 
-graph = dict()
-for cave1, cave2 in rubovi:
-        if cave1 not in graph:
-            graph[cave1] = set()
-        if cave2 not in graph:
-            graph[cave2] = set()
-        graph[cave1].add(cave2)
-        graph[cave2].add(cave1)
+adj = defaultdict(list)
 
-print("P1: ", countPath(graph, True))
-print("P2: ", countPath(graph, False))
+for i, j in podaci:
+    adj[i].append(j)
+    adj[j].append(i)
+
+
+global odg
+odg = 0
+posjeta = set()
+
+def dfs(cave):
+    global odg
+
+    if cave == "end":
+        odg += 1
+        return
+
+    if jeMalo(cave) and cave in posjeta:
+        return
+
+    if jeMalo(cave):
+        posjeta.add(cave)
+
+    # Dodaj sve susjede u red 
+    for nbr in adj[cave]:
+        if nbr == "start":
+            continue
+        dfs(nbr)
+
+    if jeMalo(cave):
+        posjeta.remove(cave)
+
+dfs("start")
+print("P1: ", odg)
+
+
+odg = 0
+posjeta = defaultdict(int)
+
+def dfs(cave):
+    global odg
+
+    if cave == "end":
+        odg += 1
+        return
+
+    if jeMalo(cave):
+        posjeta[cave] += 1
+
+        checkPosjeta = 0  
+        for i in posjeta:
+            checkPosjeta += posjeta[i] > 1
+
+            # Ni jedna mala pecina moze biti posjecena vise od 2 puta
+            if posjeta[i] > 2:
+                posjeta[cave] -= 1
+                return
+
+        if checkPosjeta > 1:
+            posjeta[cave] -= 1
+            return
+
+    # Dodaj sve susjede u red
+    for nbr in adj[cave]:
+        if nbr == "start":
+            continue
+        dfs(nbr)
+
+    if jeMalo(cave):
+        posjeta[cave] -= 1
+
+
+dfs("start")
+
+print("P2: ", odg)
